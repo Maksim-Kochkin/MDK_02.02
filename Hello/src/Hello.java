d0d50e2a463f3846de6795c930de4dbd004780a3
Maksim-Kochkin <106807060+Maksim-Kochkin@users.noreply.github.com>
2022-09-29 22:58:55 +0500

    Задание 1 и 2 за 2ю пару 30.09

---
+++ Hello/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+			System.out.println("a=1, c=a+1 m=2, n=3 решение уравнений\nb=6, d=b*2-1 p=4, g=5 нахождение корней");
+		
+	}
+public class Hello2{
+		public static void main(String[] args) {
+			for(int i = 0; i<1;i++) {
+				System.out.println("a=1, c=a+1 m=2, n=3 решение уравнений\nb=6, d=b*2-1 p=4, g=5 нахождение корней");
+			}
+			
+		}
+	}
+
+}
